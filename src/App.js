9525357fa12b814f86f4eac999d5bef457700db8
Rohan Tomar <rstomar710@gmail.com>
2021-10-27 20:55:27 +0530

    home page commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="flex flex-col w-screen h-screen dark:bg-gray-900 dark:opacity-25">
       <Header/>
+      <Home/>
       
      
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 module.exports = {
+  variants: {
+    animation: ['responsive', 'motion-safe', 'motion-reduce']
+  },
   darkMode: 'class',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
+
   plugins: [],
 }
